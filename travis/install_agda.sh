7a15242e4fcbdd260e232a8e229e25f4450b5703
Guillaume Allais <guillaume.allais@ens-lyon.org>
2018-07-03 17:59:03 +0200

    [ travis ] fix typo

--- travis/install_agda.sh
+++ travis/install_agda.sh
@@ -2,7 +2,7 @@
 if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/[^2]*//"` = "2.5.1" ]; then
   cabal update
   cabal install alex happy cpphs
-  cabal install Agda-2.54
+  cabal install Agda-2.5.4
   mkdir -p $HOME/.agda
   cp libraries-2.5.4 $HOME/.agda/
   cd $HOME/.agda/
