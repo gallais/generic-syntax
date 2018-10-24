80574ff4120b841f586ea89889c5f6f231fad604
Guillaume Allais <guillaume.allais@ens-lyon.org>
2018-10-24 15:02:45 +0200

    [ travis, cleanup ] scripts using a generic VERSION

--- travis/install_agda.sh
+++ travis/install_agda.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
-if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/[^2]*//"` = "2.5.1" ]; then
+AGDA_VERSION=2.5.4
+
+if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/[^2]*//"` = "$AGDA_VERSION" ]; then
   cabal update
   cabal install alex happy cpphs
-  cabal install Agda-2.5.4
+  cabal install Agda-"$AGDA_VERSION"
   mkdir -p $HOME/.agda
-  cp libraries-2.5.4 $HOME/.agda/
+  cp libraries-"$AGDA_VERSION" $HOME/.agda/
   cd $HOME/.agda/
   wget https://github.com/agda/agda-stdlib/archive/v0.16.tar.gz
   tar -xvzf v0.16.tar.gz
