b94bed15e0847745bef9c8b31808b79479e50e8b
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-03-15 20:38:17 +0000

    [ travis ] do not cache .agda

--- travis/install_agda.sh
+++ travis/install_agda.sh
@@ -5,11 +5,12 @@ if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/[^2]*//"` = "$AGDA_VERSION
   cabal update
   cabal install alex happy cpphs --force-reinstalls
   cabal install Agda-"$AGDA_VERSION" --force-reinstalls
-  mkdir -p $HOME/.agda
-  cp libraries-"$AGDA_VERSION" $HOME/.agda/
-  cd $HOME/.agda/
-  git clone git@github.com:agda/agda-stdlib/ agda-stdlib-0.18
-  cd agda-stdlib-0.18
-  git checkout a0bfe7422d2aa0f0f49c9647659ce34e6e741375
-  cd -
 fi
+
+mkdir -p $HOME/.agda
+cp libraries-"$AGDA_VERSION" $HOME/.agda/
+cd $HOME/.agda/
+git clone git@github.com:agda/agda-stdlib/ agda-stdlib-0.18
+cd agda-stdlib-0.18
+git checkout a0bfe7422d2aa0f0f49c9647659ce34e6e741375
+cd -
