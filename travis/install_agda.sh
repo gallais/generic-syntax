18b8e43bb5dd7d3ed04564a9113dcb1b97b42ae7
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-03-29 08:35:17 +0000

    [ admin ] backport to Agda 2.5.4.2 and stdlib 0.17
    Looks like 2.6.0 and 1.0 won't be released in time for the talk.
    So I'm rolling back the compatibility changes and sticking to the
    stable releases.

--- travis/install_agda.sh
+++ travis/install_agda.sh
@@ -5,12 +5,17 @@ if ! type "agda" > /dev/null || [ ! `agda -V | sed "s/[^2]*//"` = "$AGDA_VERSION
   cabal update
   cabal install alex happy cpphs --force-reinstalls
   cabal install Agda-"$AGDA_VERSION" --force-reinstalls
+  mkdir -p $HOME/.agda
+  cp libraries-${VERSION} $HOME/.agda/
+  cd $HOME/.agda/
+  wget https://github.com/agda/agda-stdlib/archive/v0.17.tar.gz
+  tar -xvzf v0.17.tar.gz
+  cd -
 fi
 
-mkdir -p $HOME/.agda
-cp libraries-"$AGDA_VERSION" $HOME/.agda/
-cd $HOME/.agda/
-git clone https://github.com/agda/agda-stdlib/ agda-stdlib-0.18
-cd agda-stdlib-0.18
-git checkout a0bfe7422d2aa0f0f49c9647659ce34e6e741375
-cd -
+# mkdir -p $HOME/.agda
+# cp libraries-"$AGDA_VERSION" $HOME/.agda/
+# cd $HOME/.agda/
+# git clone https://github.com/agda/agda-stdlib/ agda-stdlib-0.18
+# cd agda-stdlib-0.18
+# git checkout a0bfe7422d2aa0f0f49c9647659ce34e6e741375
