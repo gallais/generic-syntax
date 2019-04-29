46a77901a4ffb0e2284227de1ed0a6228e58edbc
Guillaume Allais <guillaume.allais@ens-lyon.org>
2019-04-29 09:12:15 +0100

    [ admin ] update src extraction from doc

--- tools/build-src.sh
+++ tools/build-src.sh
@@ -4,13 +4,13 @@
 ghc tools/LiterateToRawAgda.hs -o lit2raw
 
 # Grab the list of versioned lagda files in doc/
-TARGETS=$(git ls-tree -r --name-only HEAD doc/ | grep ".*\.lagda$")
+TARGETS=$(git ls-tree -r --name-only HEAD doc/common | grep ".*\.lagda$")
 # Convert each one of them & put it in src/
 for i in ${TARGETS}
 do
   TARGET=$(basename $i .lagda)
-  DIRECTORY=$(dirname $i | sed "s|doc||")
+  DIRECTORY=$(dirname $i | sed "s|doc/common||")
   ./lit2raw $i
   mkdir -p src/${DIRECTORY}
-  mv doc/${DIRECTORY}/${TARGET}.agda src/${DIRECTORY}
+  mv doc/common/${DIRECTORY}/${TARGET}.agda src/${DIRECTORY}
 done
